592e5a38d952d7b872f932549accd724d626a800
imranhss <emranhss@gmail.com>
2023-02-08 22:22:10 +0600

    tollbar done

--- AttendenceApp/app/src/main/java/com/example/attendenceapp/ClassAdapter.java
+++ AttendenceApp/app/src/main/java/com/example/attendenceapp/ClassAdapter.java
@@ -24,8 +24,26 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHol
         this.onItemClickedListener = onItemClickedListener;
     }
 
+//    When Click one item it will go to another Activity by this interface
     public interface OnItemClickedListener{
         void onClick(int possition);
+    }
+
+
+
+    public static class ClassViewHolder extends RecyclerView.ViewHolder {
+        TextView className, subjectName;
+
+        public ClassViewHolder(@NonNull View itemView, OnItemClickedListener onItemClickedListener) {
+            super(itemView);
+
+            className = itemView.findViewById(R.id.class_tv);
+            subjectName = itemView.findViewById(R.id.subject_tv);
+            // aDD OnClick listener and get the itemposition
+            itemView.setOnClickListener(v->onItemClickedListener.onClick(getAdapterPosition()));
+
+
+        }
     }
 
     public ClassAdapter(ArrayList<ClassItem> classItems, Context context) {
@@ -38,7 +56,8 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHol
     public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.class_item, parent, false);
 
-        return new ClassViewHolder(itemView);
+        //Return onItemClick Listener
+        return new ClassViewHolder(itemView,onItemClickedListener);
     }
 
     @Override
@@ -55,16 +74,5 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHol
     }
 
 
-    public static class ClassViewHolder extends RecyclerView.ViewHolder {
-        TextView className, subjectName;
 
-        public ClassViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            className = itemView.findViewById(R.id.class_tv);
-            subjectName = itemView.findViewById(R.id.subject_tv);
-
-
-        }
-    }
 }
--- AttendenceApp/app/src/main/java/com/example/attendenceapp/MainActivity.java
+++ AttendenceApp/app/src/main/java/com/example/attendenceapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.attendenceapp;
 
-import androidx.appcompat.app.AlertDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -17,11 +22,16 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FloatingActionButton  flotingBtnAdd;
+    private FloatingActionButton flotingBtnAdd;
     RecyclerView recyclerView;
     ClassAdapter classAdapter;
     RecyclerView.LayoutManager layoutManager;
-    ArrayList<ClassItem> classItems=new ArrayList<>();
+    ArrayList<ClassItem> classItems = new ArrayList<>();
+
+    // for new toolbar
+    Toolbar toolbar;
+    TextView title, subTitle;
+    ImageButton back, save;
 
 
     @Override
@@ -29,49 +39,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        flotingBtnAdd=findViewById(R.id.flotingBtnAdd);
+        flotingBtnAdd = findViewById(R.id.flotingBtnAdd);
 
-        flotingBtnAdd.setOnClickListener(View-> showDialog());
+        flotingBtnAdd.setOnClickListener(View -> showDialog());
 
-        recyclerView=findViewById(R.id.recycler_view);
+        recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
-        layoutManager=new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        classAdapter=new ClassAdapter( classItems, this);
+        classAdapter = new ClassAdapter(classItems, this);
         recyclerView.setAdapter(classAdapter);
+        toolbar = findViewById(R.id.toolbar);
+        title = toolbar.findViewById(R.id.title_toolbar);
+        subTitle = toolbar.findViewById(R.id.subtitle_toolbar);
+
+        back=toolbar.findViewById(R.id.back);
+        save=toolbar.findViewById(R.id.save);
+
+        title.setText("Attendence App");
+        subTitle.setVisibility(View.INVISIBLE);
+        back.setVisibility(View.INVISIBLE);
+        save.setVisibility(View.INVISIBLE);
+
+        //when we click on item it will go to another activity by this method
+        classAdapter.setOnItemClickedListener(position -> goToItemActivity(position));
 
 
     }
 
-    private void showDialog(){
-        MyDialog dialog=new MyDialog();
+    private void goToItemActivity(int position) {
+        Intent intent = new Intent(getApplicationContext(), StudentActivity.class);
+        // want to show Class name and subject name to new activity
+        intent.putExtra("className", classItems.get(position).getClassName());
+        intent.putExtra("subjectName", classItems.get(position).getSubjectName());
+        intent.putExtra("position", position);
+        startActivity(intent);
+
+
+    }
+
+    private void showDialog() {
+        MyDialog dialog = new MyDialog();
         dialog.show(getSupportFragmentManager(), MyDialog.CLASS_ADD_DIALOG);
-        dialog.setListener((className, subjectName)->addClass(className, subjectName));
+        dialog.setListener((className, subjectName) -> addClass(className, subjectName));
 
-
-//        AlertDialog.Builder builder=new AlertDialog.Builder(this);
-//        View view= LayoutInflater.from(this).inflate(R.layout.dialog, null);
-//        builder.setView(view);
-//        AlertDialog dialog= builder.create();
-//        dialog.show();
-//
-//        class_edit=view.findViewById(R.id.edt01);
-//        subject_edit=view.findViewById(R.id.edt02);
-//        Button cancel=view.findViewById(R.id.cancel_btn);
-//        Button add=view.findViewById(R.id.add_btn);
-//
-//        cancel.setOnClickListener(v-> dialog.dismiss());
-//        add.setOnClickListener(v-> {
-//            addClass();
-//            dialog.dismiss();
-//        });
-
-//        builder.create().show();
     }
 
 
     private void addClass(String className, String subjectName) {
-        classItems.add(new ClassItem(className,subjectName));
+        classItems.add(new ClassItem(className, subjectName));
         classAdapter.notifyDataSetChanged();
 
     }
